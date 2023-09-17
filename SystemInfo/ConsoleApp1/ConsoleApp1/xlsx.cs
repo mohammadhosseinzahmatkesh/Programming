c530d030bdeecbf67577d6e81e454f82419aab2d
m.zahmatkesh <mohammadhzn00@gmail.com>
2023-09-17 19:10:25 +0330

    Ubdate excel mood

---
+++ SystemInfo/ConsoleApp1/ConsoleApp1/xlsx.cs
@@ -0,0 +1,36 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.IO;
+using Worksheet = Bytescout.Spreadsheet.Worksheet;
+using System.Management;
+using Bytescout.Spreadsheet;
+
+namespace ConsoleApp1
+{
+    class xlsx
+    {
+        public void excel()
+        {
+            Spreadsheet document = new Spreadsheet();
+            Worksheet sheet = document.Workbook.Worksheets.Add("writExcelDemo");
+            sheet.Cell("A1").Value = "bbb";
+            sheet.Cell("B1").Value = "aaa";
+            if (File.Exists(System.Environment.CurrentDirectory + "-" + "SystemInfo.xlsx"))
+            {
+                File.Delete(System.Environment.CurrentDirectory + "-" + "SystemInfo.xlsx");
+            }
+            document.SaveAs(System.Environment.CurrentDirectory + "-" + "SystemInfo.xlsx");
+            document.Close();
+
+        }
+    }
+}
+
+
