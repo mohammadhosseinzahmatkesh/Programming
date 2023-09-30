591cae4e38069f2ee6736fbec8054cd146186a4f
m.zahmatkesh <mohammadhzn00@gmail.com>
2023-09-30 15:56:28 +0330

    finish

---
+++ Class/{get,set}/{get,set}/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _get_set_
+{
+    class Person
+    {
+        public string Name  // property
+        { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
---
+++ Methods/Create a Method/Create a Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_a_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+        private static void MyMethod()
+        {
+            string fname = Console.ReadLine();
+            string AGE = Console.ReadLine();
+            int age = Convert.ToInt32(AGE);
+            Console.WriteLine(fname + " is " + age);
+        }
+    }
+}
