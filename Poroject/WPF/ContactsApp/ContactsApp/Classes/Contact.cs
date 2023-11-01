67c94e1cf5e4e8f935e940fdd37dab6e2598d9f1
m.zahmatkesh <mohammadhzn00@gmail.com>
2023-11-01 15:46:14 +0330

    project of WPF

---
+++ Poroject/WPF/ContactsApp/ContactsApp/Classes/Contact.cs
@@ -0,0 +1,30 @@
+﻿using ServiceStack.DataAnnotations;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoIncrementAttribute = SQLite.AutoIncrementAttribute;
+using PrimaryKeyAttribute = SQLite.PrimaryKeyAttribute;
+
+namespace ContactsApp.Classes
+{
+    public class Contact
+    {
+        [PrimaryKey , AutoIncrement]
+        public int Id { get; set; }
+
+        public  string Name { get; set; }
+
+        public  string EmailAddress { get; set; }
+        
+        public  string Phone { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Name} - {EmailAddress} - {Phone}";
+        }
+    }
+}
